b291d58987c92971fe4c272ba2ee916652971de3
Kerron Donawa <kerrondonawa@gmail.com>
2021-02-08 17:45:13 -0400

    Added dfs implementation of collect files

--- runner.js
+++ runner.js
@@ -37,7 +37,7 @@ class Runner {
     }
 
     async collectFiles(targetPath){
-        return await this.collectFilesBFS(targetPath);
+        return await this.collectFilesDFS(targetPath);
     }
     async collectFilesBFS(targetPath){
         const files = await fs.promises.readdir(targetPath);
@@ -58,6 +58,20 @@ class Runner {
         }
     }
     
+    async collectFilesDFS(targetPath){
+        const files = await fs.promises.readdir(targetPath);
+
+        for (const file of files) {
+            const filePath = path.join(targetPath, file);
+            const stats = await fs.promises.lstat(filePath);
+            if(stats.isFile() && file.includes('.test.js')){
+                this.testFiles.push({pathName: filePath, fileName: file});
+            }
+            else if(stats.isDirectory() && !forbiddenDirs.includes(file)){
+                await this.collectFilesDFS(filePath);
+            }
+        }
+    }
 };
 
 module.exports = new Runner();
